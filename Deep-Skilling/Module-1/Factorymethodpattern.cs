92adfe948ad75a0eeb2de9b48554310966a1579c
Nekkanti Yamini Vidya <vidyanekkanti2005@gmail.com>
2026-06-14 21:11:46 +0530

    Add files via upload

---
+++ Deep-Skilling/Module-1/Factorymethodpattern.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+interface IShape
+{
+    void Draw();
+}
+
+class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing Circle");
+    }
+}
+
+class Rectangle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing Rectangle");
+    }
+}
+
+class ShapeFactory
+{
+    public static IShape GetShape(string shape)
+    {
+        if (shape == "Circle")
+            return new Circle();
+        return new Rectangle();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IShape shape = ShapeFactory.GetShape("Circle");
+        shape.Draw();
+    }
+}
