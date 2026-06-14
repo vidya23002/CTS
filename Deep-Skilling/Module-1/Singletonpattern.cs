574e01395bf7614e4864bc6a69762e0bf48c537f
Nekkanti Yamini Vidya <vidyanekkanti2005@gmail.com>
2026-06-14 19:31:55 +0530

    Add files via upload

---
+++ Deep-Skilling/Module-1/Singletonpattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public sealed class Singleton
+{
+    private static Singleton instance = null;
+    private Singleton()
+    {
+        Console.WriteLine("Singleton Instance Created");
+    }
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Singleton obj1 = Singleton.GetInstance();
+        Singleton obj2 = Singleton.GetInstance();
+
+        Console.WriteLine(obj1 == obj2);
+    }
+}
