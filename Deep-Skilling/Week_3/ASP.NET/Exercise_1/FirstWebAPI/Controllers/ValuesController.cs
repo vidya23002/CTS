d2c9675070626269ffae6c8a6a2935b1dcc12b49
Nekkanti Yamini Vidya <vidyanekkanti2005@gmail.com>
2026-07-17 09:34:44 +0530

    Add files via upload

---
+++ Deep-Skilling/Week_3/ASP.NET/Exercise_1/FirstWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace FirstWebApi.Controllers
+{ 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[]
+            {
+                "Apple",
+                "Banana",
+                "Orange"
+            });
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"ID = {id}");
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok($"Added : {value}");
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok($"Updated ID = {id} with Value = {value}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleted ID = {id}");
+        }
+    }
+}
