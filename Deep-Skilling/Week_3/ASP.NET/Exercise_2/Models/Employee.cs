3a5a6e73f4a4b2142416e70803935a6c878cf601
Nekkanti Yamini Vidya <vidyanekkanti2005@gmail.com>
2026-07-18 08:34:51 +0530

    Add files via upload

---
+++ Deep-Skilling/Week_3/ASP.NET/Exercise_2/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public decimal Salary { get; set; }
+    }
+}
