0d8b212515071c616d0eac051f637df4bf342f50
Nekkanti Yamini Vidya <vidyanekkanti2005@gmail.com>
2026-07-18 08:28:18 +0530

    Add files via upload

---
+++ Deep-Skilling/Week_4/Microservices/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using JWTAuthenticationAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthenticationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            if (model.Username == "shamal" &&
+                model.Password == "shamal123")
+            {
+                var token = GenerateJwtToken(model.Username);
+
+                return Ok(new
+                {
+                    Token = token
+                });
+            }
+
+            return Unauthorized("Invalid Username or Password");
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var key = new SymmetricSecurityKey(
+    Encoding.UTF8.GetBytes("ThisIsMyVeryStrongSecretKeyForJwtAuthentication123456789"));
+
+            var creds = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "MyAuthServer",
+                audience: "MyApiUsers",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler()
+                .WriteToken(token);
+        }
+    }
+}
---
+++ Deep-Skilling/Week_4/Microservices/Controllers/SecureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthenticationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet("data")]
+        [Authorize]
+        public IActionResult GetSecureData()
+        {
+            return Ok("This is protected data.");
+        }
+    }
+}
