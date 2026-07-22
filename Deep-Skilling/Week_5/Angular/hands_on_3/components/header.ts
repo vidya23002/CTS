c4ffec6dc7e4d6276b2207657a704a05e68a6c8e
Nekkanti Yamini Vidya <vidyanekkanti2005@gmail.com>
2026-07-22 11:09:51 +0530

    Add files via upload

---
+++ Deep-Skilling/Week_5/Angular/hands_on_3/components/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class HeaderComponent {
+}
