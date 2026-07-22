aab907c9b0eb265e10879387f4a6dbe343ef3a34
Nekkanti Yamini Vidya <vidyanekkanti2005@gmail.com>
2026-07-22 11:11:25 +0530

    Add files via upload

---
+++ Deep-Skilling/Week_5/Angular/hands_on_3/pipes/credit-label-pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'creditLabel'
+})
+export class CreditLabelPipe implements PipeTransform {
+
+  transform(value: number | null): string {
+
+    if (value === null || value === 0) {
+      return 'No Credits';
+    }
+
+    if (value === 1) {
+      return '1 Credit';
+    }
+
+    return value + ' Credits';
+  }
+}
